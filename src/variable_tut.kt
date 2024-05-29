5b57e3bf843c2b9125d8b6d3503ec24879d84268
JayPanchal013 <202001253@daiict.ac.in>
2024-05-29 12:33:12 +0530

    first

---
+++ src/variable_tut.kt
@@ -0,0 +1,12 @@
+fun main() {
+//this is used when we dont want the value to change in future( kin dof constant)
+    val name = "khushi"
+
+    // this is used when we want to update the value in future
+    var v = "khushi"
+    v = "trivedi"
+
+    println(name + v)
+}
+
+// output will khushitrivedi
